f5775a3fa784976e0c3463bed8a3f618cb82f15e
dongrerohan421 <dongrerohan421@gmail.com>
2020-10-19 18:32:27 -0400

    First commit

---
+++ src/test/java/com/mockito/FirstMockitoTest.java
@@ -0,0 +1,14 @@
+package com.mockito;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class FirstMockitoTest {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
